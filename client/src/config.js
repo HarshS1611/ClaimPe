d815d50fd71760110f9ddd85635db3d07e24cfef
HarshS1611 <harshsingh16d@gmail.com>
2023-07-02 19:02:49 +0530

    contract compiled and deployed on mumbai testnet

---
+++ client/src/config.js
@@ -0,0 +1,2 @@
+export const  transactionsAddress = 0xB73C933F84DD272972Fd247aDACA670C35d52179 ;
+export const  stakeAddress =  0x455b40680BBCa3565b737D4ddBc55D6439DD933A ;
--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,37 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
+const API_KEY = process.env.API_KEY;
+const MNEMONIC = process.env.MNEMONIC;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  compilers: {
+    solc: {
+      version: "0.8.0", // Specify the desired solc version here
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
+  },  
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
     },
+    mumbai: {
+      provider: () => new HDWalletProvider(mnemonic, `https://polygon-mumbai.g.alchemy.com/v2/${API_KEY}`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
   },
 };
